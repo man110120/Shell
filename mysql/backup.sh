25cda1b75d82fb251325b5c13b377f8467eb9434
Peter Wang <19649959@qq.com>
2016-09-27 14:54:02 +0800

    changed backup.sh

--- mysql/backup.sh
+++ mysql/backup.sh
@@ -20,12 +20,8 @@ echo "creat2 ok"
 fi
 
 cd $DIR2
-
-rm -rf $DIR2/test.sql
-echo "show tables;" >> $DIR2/test.sql
-
-rm -rf $DIR2/table.txt
-/data/dbserver/mysql/bin/mysql -uroot -p${mysql_pwd} portal < $DIR2/test.sql >> $DIR2/table.txt
+echo "show tables;" > $DIR2/test.sql
+/data/dbserver/mysql/bin/mysql -uroot -p${mysql_pwd} portal < $DIR2/test.sql > $DIR2/table.txt
 
 #分表导出sql
 while read Table
@@ -44,21 +40,18 @@ do
 done
 /bin/rm -rf $DIR2/*.sql
 
-#分库备份
+#备份所有数据库(不包含默认数据库)
 cd $DIR
 /data/dbserver/mysql/bin/mysql -uroot -p${mysql_pwd}  -e "show databases;" -N  >dbName.txt
-#/bin/sed -i '/mysql/d' dbName.txt
-#/bin/sed -i '/test/d' dbName.txt
-#/bin/sed -i '/information_schema/d' dbName.txt
 
 for i in mysql infomation_schema performance_schema
 do
  /bin/sed -i "/$i/d" dbName.txt
 done
 
-for a in `cat dbName.txt`
+for i in `cat dbName.txt`
 do
-/data/dbserver/mysql/bin/mysqldump -uroot -p${mysql_pwd} --opt $a > $DIR/$a.sql
+/data/dbserver/mysql/bin/mysqldump -uroot -p${mysql_pwd} --opt $i > $DIR/$i.sql
 done
 #将portal.sql打包成portal.tar.gz，将其它打包成other.tar.gz。打包完成后，删除所有.sql文件
 /bin/tar -czf portal.tar.gz portal.sql
