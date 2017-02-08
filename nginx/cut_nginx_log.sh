c3162ba0f36fd1f34594f36cbacb5b7d5d8afe5d
Peter Wang <19649959@qq.com>
2017-02-08 16:58:10 +0800

    change nginx

--- nginx/cut_nginx_log.sh
+++ nginx/cut_nginx_log.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-# This script run at 00:00,每天0点开始切割日志
+# 每天0点开始切割日志
 #添加定时执行任务 0 0 * * * /bin/bash  /data/webserver/nginx/sbin/cut_nginx_log.sh
-
-# The Nginx logs path
-logs_path="/data/webserver/nginx/logs/"
-mkdir -p ${logs_path}$(date -d "yesterday" +"%Y")/$(date -d "yesterday" +"%m")/
-mv ${logs_path}access.log ${logs_path}$(date -d "yesterday" +"%Y")/$(date -d "yesterday" +"%m")/access_$(date -d "yesterday" +"%Y%m%d").log
+path="/data/webserver/nginx/logs/"
+logs_path=${path}$(date +%Y)/$(date +%Y%m)
+if [ ! -d ${logs_path} ];then
+mkdir -p ${logs_path}
+fi
+mv ${path}access.log ${logs_path}/access_$(date +%Y%m%d).log
 /data/webserver/nginx/sbin/nginx -s reload
