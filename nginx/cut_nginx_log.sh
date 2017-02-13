df0ba4b68c789f1c8ee872318a22bf54202a2b2e
Peter Wang <19649959@qq.com>
2017-02-13 17:23:13 +0800

    fix a bug

--- nginx/cut_nginx_log.sh
+++ nginx/cut_nginx_log.sh
@@ -2,7 +2,7 @@
 # 每天0点开始切割日志
 #添加定时执行任务 0 0 * * * /bin/bash  /data/webserver/nginx/sbin/cut_nginx_log.sh
 path="/data/webserver/nginx/logs/"
-logs_path=${path}$(date -d last-day +%Y)/$(date -d lastday+%Y%m)
+logs_path=${path}$(date -d last-day +%Y)/$(date -d last-day+%Y%m)
 if [ ! -d ${logs_path} ];then
 mkdir -p ${logs_path}
 fi
